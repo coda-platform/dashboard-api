daf861e52c6fa73fefd5d106d51b8664b6e3e85e
Louis Mullie <louis.mullie@gmail.com>
2023-10-25 00:56:49 -0400

    Update keycloak-config.js

--- src/auth/keycloak-config.js
+++ src/auth/keycloak-config.js
@@ -2,7 +2,7 @@ const config = {
     "realm": process.env.CODA_DASHBOARD_API_AUTH_REALM,
     "auth-server-url": process.env.CODA_AUTH_SERVICE_URL,
     "ssl-required": "external",
-    "resource": "coda-dashboard-api",
+    "resource": "dashboard-api",
     "credentials": {
         "secret": process.env.CODA_DASHBOARD_API_AUTH_CLIENT_SECRET
     },
